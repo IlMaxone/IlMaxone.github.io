75bd5d999891f30ef0d32929e126d378f75f1ab8
IlMaxone <maxo.paxo43@gmail.com>
2023-03-16 09:57:57 +0100

    test

---
+++ index.js
@@ -0,0 +1,38 @@
+var aggiungiPiatti = document.querySelector('.aggiungi-piatto');
+var piatti = document.querySelector('.piatti');
+var listaOrdini = JSON.parse(localStorage.getItem('ordini')) || [];
+var cancellaPiatti = document.querySelector('.reset-piatti');
+aggiungiPiatti.addEventListener('submit', aggiungiPiatto);
+cancellaPiatti.addEventListener('click', cancellaPiatto);
+function aggiungiPiatto(e) {
+    e.preventDefault();
+    var nome = (this.querySelector('[name="piatto"]')).value;
+    var piatto = {
+        nome: nome,
+        portato: false
+    };
+    listaOrdini.push(piatto);
+    popolaLista();
+    localStorage.setItem('ordini', JSON.stringify(listaOrdini));
+    this.reset();
+}
+function cancellaPiatto() {
+    localStorage.clear;
+    popolaLista();
+}
+function popolaLista() {
+    piatti.innerHTML = listaOrdini.map(function (ordine, index) {
+        return "\n        <li>\n        <input type=\"checkbox\" data-index=\"".concat(index, "\" id=\"item").concat(index, "\" ").concat(ordine.portato ? 'checked' : '', " />\n        <label for=\"item").concat(index, "\">").concat(ordine.nome, "</label>\n        </li>\n        ");
+    }).join('');
+}
+piatti.addEventListener('click', togglePortato);
+function togglePortato(e) {
+    var el = e.target;
+    if (el.matches('input')) {
+        var index = Number(el.dataset.index);
+        listaOrdini[index].portato = !listaOrdini[index].portato;
+        localStorage.setItem('ordini', JSON.stringify(listaOrdini));
+        popolaLista();
+    }
+}
+popolaLista();
---
+++ index.ts
@@ -0,0 +1,56 @@
+interface Piatto {
+    nome: string
+    portato: boolean
+}
+
+const aggiungiPiatti = document.querySelector('.aggiungi-piatto') as HTMLFormElement;
+const piatti = document.querySelector('.piatti') as HTMLElement;
+const listaOrdini: Piatto[] = JSON.parse(localStorage.getItem('ordini')!) || [];
+const cancellaPiatti = document.querySelector('.reset-piatti') as HTMLElement;
+
+aggiungiPiatti.addEventListener('submit', aggiungiPiatto);
+cancellaPiatti.addEventListener('click', cancellaPiatto);
+
+function aggiungiPiatto(this: any, e: Event){
+    e.preventDefault();
+    const nome = (this.querySelector('[name="piatto"]')).value;
+    const piatto = {
+        nome,
+        portato: false
+    }
+    listaOrdini.push(piatto);
+    popolaLista();
+    localStorage.setItem('ordini', JSON.stringify(listaOrdini));
+    this.reset();
+}
+
+function cancellaPiatto(){
+    localStorage.clear;
+    popolaLista();
+}
+
+function popolaLista(){
+    piatti.innerHTML = listaOrdini.map((ordine, index) => {
+        return `
+        <li>
+        <input type="checkbox" data-index="${index}" id="item${index}" ${ordine.portato ? 'checked' : ''} />
+        <label for="item${index}">${ordine.nome}</label>
+        </li>
+        `
+    }).join('');
+}
+
+piatti.addEventListener('click', togglePortato);
+
+function togglePortato(e: MouseEvent){
+    const el = e.target! as HTMLLIElement;
+
+    if(el.matches('input')){
+        const index = Number(el.dataset.index);
+        listaOrdini[index].portato = !listaOrdini[index].portato;
+        localStorage.setItem('ordini', JSON.stringify(listaOrdini));
+        popolaLista();
+    }
+}
+
+popolaLista();
